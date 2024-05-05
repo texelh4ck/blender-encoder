67da28c2910389a307dda5911c8996ad60cf01af
TexelHack <danlab.licea@gmail.com>
2024-05-05 13:38:47 -0400

    feat: Generate a preview for all

--- static/js/menu.js
+++ static/js/menu.js
@@ -20,14 +20,29 @@ const bt_preview = async() => {
         return
     }
     
-    rendering.style.visibility = "visible";
-    render_name.innerHTML = "Preview: " + blend_data(selected.title).name
-
+    document.getElementById("bt_preview").ariaBusy = true;
+    
     const file = blend_data(selected.title);
     await render_frame(file.filepath)
+
+    document.getElementById("bt_preview").ariaBusy = false;
     
-    rendering.style.visibility = "hidden";
     set_preview(selected.title)
+}
+
+const bt_all_preview = async() => {
+    const BlendList = blend_get();
+
+    document.getElementById("bt_all_preview").ariaBusy = true;
+
+    for (let i = 0; i < BlendList.length; i++) {
+        const file = blend_data(BlendList[i]);
+        await render_frame(file.filepath)
+        set_preview(selected.title)
+    }
+    
+    document.getElementById("bt_all_preview").ariaBusy = false;
+    
 }
 
 const bt_import = () => {
